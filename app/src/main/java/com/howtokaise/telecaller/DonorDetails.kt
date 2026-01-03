6889adfdf3df6469e9c51bd24e88201dcb1c0775
Adil Jawed <mdadiljawed786@gmail.com>
2026-01-03 22:19:50 +0530

    Donor Details

---
+++ app/src/main/java/com/howtokaise/telecaller/DonorDetails.kt
@@ -0,0 +1,20 @@
+package com.howtokaise.telecaller
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DonorDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_donor_details)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
